994983d751445029ecee5dfc6b3dd41f46f6a99e
Tifany <67019104+tiffany-m@users.noreply.github.co>
2021-01-06 16:41:15 -0500

    changes

---
+++ src/shared/fortunesData.js
@@ -0,0 +1,38 @@
+export const FORTUNES = [
+  {
+    fortune: 'A dubious friend may be an enemy in camouflage.',
+  },
+  {
+    fortune: 'Because you demand more from yourself, others respect you deeply.',
+  },
+  {
+    fortune: 'Curiosity kills boredom. Nothing can kill curiosity.',
+  },
+  {
+    fortune: 'Disbelief destroys the magic.',
+  },
+  {
+    fortune: 'Fear and desire – two sides of the same coin.',
+  },
+  {
+    fortune: 'Good to begin well, better to end well.',
+  },
+  {
+    fortune: 'Help! I’m being held prisoner in a chinese bakery!',
+  },
+  {
+    fortune: 'It is better to be an optimist and proven a fool than to be a pessimist and be proven right.',
+  },
+  {
+    fortune: 'Man’s mind, once stretched by a new idea, never regains it’s original dimensions.',
+  },
+  {
+    fortune: 'The best prediction of future is the past.',
+  },
+  {
+    fortune: 'What’s hidden in an empty box?',
+  },
+  {
+    fortune: 'You can see a lot just by looking.',
+  },
+];
